4265e0254ca2ba7d2dc772c065d46bbeea4fa459
LMETRIX214 <68071760+LMETRIX214@users.noreply.github.com>
2022-11-17 17:06:24 -0600

    Avance

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import NavBar from "./components/Nav/NavBar";
+import "./App.css";
+import React, { Component } from 'react';
+
+import ItemListContainter from "./components/ItemList/ItemListContainter";
+import { Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import HomeContent from "./components/HomeContent";
+import CartWidget from "./components/Cart/CartWidget";
+import CartContainer from "./components/Cart/CartContainer";
+import ItemDetailContainer from "./components/ItemDetail/ItemDetailContainer";
+
+function App() {
+  const storeInfo = {
+    storeName: "FASTMARKET",
+    categories: [
+      { id: 1, title: "Computadoras", link: "/computers" },
+      { id: 2, title: "Laptops", link: "/laptops" },
+      { id: 3, title: "Monitores", link: "/monitors" },
+      { id: 4, title: "Accesorios", link: "/accesories" },
+    ],
+  };
+
+  return (
+    <>
+      <NavBar storeInfo={storeInfo} />
+      <div className="cart-widget">
+        <CartWidget></CartWidget>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <HomeContent />
+        </Route>
+        <Route exact path="/cart">
+          <CartContainer />
+        </Route>
+
+        {storeInfo.categories.map((elem, idx) => (
+          <Route exact path={elem.link} key={idx}>
+            <ItemListContainter data={elem.title} />
+          </Route>
+        ))}
+
+        {storeInfo.categories.map((elem, idx) => (
+          <Route path={`${elem.link}/:itemID`} key={idx}>
+            <ItemDetailContainer />
+          </Route>
+        ))}
+      </Switch>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ItemDetail/ItemCount.js
@@ -0,0 +1,65 @@
+// Entregable 04 ->
+import { useState } from "react";
+import { InputGroup, Button, FormControl } from "react-bootstrap";
+import React, { Component } from 'react';
+
+function ItemCount(props) {
+  const { stock, initial, onAdd } = props.props;
+
+  // Inicializamos los hooks y pasamos variables
+  const [cantidad, setCantidad] = useState(initial);
+  const [actualStock, setActualStock] = useState(stock);
+
+  // Logica de suma y resta
+  const add = () => {
+    if (cantidad <= stock - 1) {
+      setCantidad(cantidad + 1);
+    }
+  };
+
+  const substract = () => {
+    if (cantidad > initial) {
+      setCantidad(cantidad - 1);
+    }
+  };
+
+  return (
+    <div>
+      <h5>Cantidad disponible: {actualStock}</h5>
+      <hr />
+      <div>
+        <InputGroup className="mb-3 form-calculator-minmax">
+          <Button
+            variant="outline-secondary"
+            id="button-addon1"
+            onClick={substract}
+          >
+            -
+          </Button>
+          <FormControl
+            aria-label="Valor de stock a agregar"
+            aria-describedby="basic-addon1"
+            value={cantidad}
+            readOnly
+          />
+          <Button variant="outline-secondary" id="button-addon1" onClick={add}>
+            +
+          </Button>
+        </InputGroup>
+        <Button
+          variant="primary"
+          onClick={() => {
+            actualStock >= cantidad &&
+              setActualStock(onAdd(actualStock, cantidad));
+            cantidad > actualStock &&
+              alert("No queda más stock, no se puede agregar al carrito!");
+          }}
+        >
+          Agregar al carrito
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ItemCount;
